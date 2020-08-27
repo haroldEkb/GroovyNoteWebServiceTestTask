6a1c8126c8639b544501a78a7f5728198882f839
Vladimir Gorin <haroldEkb@mail.ru>
2020-08-27 16:04:23 +0500

    Create basic project structure

---
+++ src/main/groovy/com/haroldekb/GroovyNoteWebServiceTestTask/repository/NoteRepository.groovy
@@ -0,0 +1,9 @@
+package com.haroldekb.GroovyNoteWebServiceTestTask.repository
+
+import com.haroldekb.GroovyNoteWebServiceTestTask.entity.Note
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface NoteRepository extends JpaRepository<Note, Integer> {
+}
